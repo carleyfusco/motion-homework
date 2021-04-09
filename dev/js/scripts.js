f64ca5dc23cc7451107dec5827ff0691ad874f18
carleyfusco <63366527+carleyfusco@users.noreply.github.com>
2021-04-08 22:43:47 -0400

    Addition1

---
+++ dev/js/scripts.js
@@ -0,0 +1,19 @@
+import { gsap } from "gsap";
+
+// gsap.to("#red-circle", { duration: 4, x: 200 });
+
+// gsap.from("#blue-square", { duration: 4, scale: 2, alpha: 0, delay: 2, repeat: -1, yoyo: true });
+
+
+let shapeTL = gsap.timeline();
+
+shapeTL.to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-1, yoyo: true, repeat:5})
+    .to("#purple-circle", {duration: 1, scaleX:-5, scaleY:-1})
+    .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-5})
+    .to("#purple-circle", {duration: 1, scaleX:-5, scaleY:-1})
+    .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-5})
+    .to("#purple-circle", {duration: .5, scaleX:50, scaleY:50})
+    .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-1})
+    // from("#red-cricle", {duration: 1, x:-300}, "moveShapes")
+    // .to("#red-circle", {duration: 1, x:200, y:-200})
+    // .to("#red-circle", {duration: 1, x:-600, y:600})
