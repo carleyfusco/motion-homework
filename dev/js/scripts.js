c00c713b633a411a0e754a3807d7c7b02ebe599f
carleyfusco <63366527+carleyfusco@users.noreply.github.com>
2021-04-09 20:43:39 -0400

    Round 2

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,6 +14,11 @@ shapeTL.to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-1, yoyo: true, rep
     .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-5})
     .to("#purple-circle", {duration: .5, scaleX:50, scaleY:50})
     .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-1})
+    .to("#purple-circle", {duration: 3, alpha: 0})
+    .to("#purple-circle", {duration: 3, alpha: 1})
+    .to("#purple-circle", { duration: 1, ease: "bounce.out", y:200, x:400})
+    .to("#purple-circle", { duration: 2.5, ease: "power2.out", y: -300, scale:2});
+
     // from("#red-cricle", {duration: 1, x:-300}, "moveShapes")
     // .to("#red-circle", {duration: 1, x:200, y:-200})
     // .to("#red-circle", {duration: 1, x:-600, y:600})
