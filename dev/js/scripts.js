c0be4705ac59b75edade9bb0ec10ba4782cb3aee
carleyfusco <63366527+carleyfusco@users.noreply.github.com>
2021-04-12 19:45:42 -0400

    Round3
    Final addition

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,12 +12,25 @@ shapeTL.to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-1, yoyo: true, rep
     .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-5})
     .to("#purple-circle", {duration: 1, scaleX:-5, scaleY:-1})
     .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-5})
-    .to("#purple-circle", {duration: .5, scaleX:50, scaleY:50})
+    .to("#purple-circle", {duration: 1, scaleX:50, scaleY:50})
     .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-1})
-    .to("#purple-circle", {duration: 3, alpha: 0})
-    .to("#purple-circle", {duration: 3, alpha: 1})
+    .to("#purple-circle", {duration: 1, alpha: 0})
+    .to("#purple-circle", {duration: 1, backgroundColor: "#FFD700"})
+    .to("#purple-circle", {duration: 1, alpha: 1})
     .to("#purple-circle", { duration: 1, ease: "bounce.out", y:200, x:400})
-    .to("#purple-circle", { duration: 2.5, ease: "power2.out", y: -300, scale:2});
+    .to("#purple-circle", { duration: 1, ease: "power2.out", y: -300, scale:2})
+    .to("#purple-circle", { duration: 1, ease: "elastic.out", y: 100, x:400, scale:2})
+    .to("#purple-circle", { duration: 1, ease: "steps(11)", x: -500})
+    .to("#purple-circle", {duration: 1, scaleX:-1, scaleY:-1})
+    .to("#purple-circle", { duration: 1, ease: "back.out(1.7)", y: -300 })
+    .to("#purple-circle", { duration: 1, ease: "elastic.out(1, 0.3)", y:300 })
+    .to("#purple-circle", {duration: 1, backgroundColor: "#191970"})
+    .to("#purple-circle", { duration: 1, ease: "bounce.out", y:-1, x:-1})
+    .to("#purple-circle", {duration: 1, scaleX:50, scaleY:50});
+    
+
+
+    
 
     // from("#red-cricle", {duration: 1, x:-300}, "moveShapes")
     // .to("#red-circle", {duration: 1, x:200, y:-200})
